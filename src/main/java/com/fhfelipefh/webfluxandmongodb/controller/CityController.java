c44fa1cd39ed8c85848d18b388cbb9a5515093b5
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2022-06-07 21:10:39 -0300

    Update CityController.java
    Removendo importação desnecessária.

--- src/main/java/com/fhfelipefh/webfluxandmongodb/controller/CityController.java
+++ src/main/java/com/fhfelipefh/webfluxandmongodb/controller/CityController.java
@@ -1,18 +1,15 @@
 package com.fhfelipefh.webfluxandmongodb.controller;
 
-import com.fhfelipefh.webfluxandmongodb.config.Generated;
 import com.fhfelipefh.webfluxandmongodb.document.City;
 import com.fhfelipefh.webfluxandmongodb.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@Controller
 public class CityController {
 
     @Autowired
